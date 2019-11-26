b167e9d156d135590ce8d12db76e7256f918b3a0
kleibermaluquish <maluquish.19@gmail.com>
2019-11-26 18:45:54 -0500

    Se configuro EnableDiscveryClient

--- src/main/java/com/empresa/app/familyrelative/ServiceFamilyRelativeApplication.java
+++ src/main/java/com/empresa/app/familyrelative/ServiceFamilyRelativeApplication.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 
 import com.empresa.app.familyrelative.model.dao.IFamilyRelativeDao;
 import com.empresa.app.familyrelative.model.document.FamilyRelative;
@@ -15,11 +17,15 @@ import com.empresa.app.familyrelative.model.document.FamilyRelative;
 import reactor.core.publisher.Flux;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ServiceFamilyRelativeApplication implements CommandLineRunner {
 
 	private static final Logger log = LoggerFactory.getLogger(ServiceFamilyRelativeApplication.class);
 	@Autowired
 	private IFamilyRelativeDao dao;
+	
+	@Autowired
+	private ReactiveMongoTemplate mongoTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceFamilyRelativeApplication.class, args);
@@ -27,6 +33,8 @@ public class ServiceFamilyRelativeApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		mongoTemplate.dropCollection("FalimyRelative").subscribe();
 
 		Flux.just(
 				new FamilyRelative("Alejandro", "Toribio", "M", new Date(), "Dni", "12345678", "Padre"),
